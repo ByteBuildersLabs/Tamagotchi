ad55225979e30ea95f546c9ad9349b00c316b16f
jimenezz22 <ljimenezbogantes@gmail.com>
2025-01-31 22:09:09 -0600

    Remove unused icons and simplify beast display in Bag component

--- client/src/components/Bag/index.tsx
+++ client/src/components/Bag/index.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import { SDK } from "@dojoengine/sdk";
 import { Link } from 'react-router-dom';
 import { Schema } from "../../dojo/bindings.ts";
-import { Swords, ShieldPlus, TestTubeDiagonal, CircleGauge } from 'lucide-react';
 import initials from "../../data/initials.tsx";
 import ControllerConnectButton from "../CartridgeController/ControllerConnectButton.tsx";
 import './main.css';
@@ -51,35 +50,6 @@ function Bag({ sdk }: { sdk: SDK<Schema> }) {
         <Link to={`/play`} className="beast" onClick={() => (document.querySelector('.navbar-toggler') as HTMLElement)?.click()}>
           <div className="beast-pic d-flex align-items-end">
             <img src={initials[beast.specie - 1].idlePicture} alt="beast" />
-            <h4 className="d-flex">
-              <span>{beast.level}</span> Lvl
-            </h4>
-          </div>
-          <div className="data">
-            <div className="item">
-              <div>
-                <Swords />
-                <span>{Math.round(beast.attack)}</span>
-              </div>
-            </div>
-            <div className="item">
-              <div>
-                <ShieldPlus />
-                <span>{Math.round(beast.defense)}</span>
-              </div>
-            </div>
-            <div className="item">
-              <div>
-                <CircleGauge />
-                <span>{Math.round(beast.speed)}</span>
-              </div>
-            </div>
-            <div className="item">
-              <div>
-                <TestTubeDiagonal />
-                <span>{beast.experience}</span>
-              </div>
-            </div>
           </div>
         </Link>
       );
