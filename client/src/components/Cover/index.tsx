1985702a7f4ec5a48c91eb94fcd494e80051de1d
Joel Vargas <vermudo.com@gmail.com>
2025-02-02 22:18:07 -0600

    style: improve code formatting and readability in Cover component

--- client/src/components/Cover/index.tsx
+++ client/src/components/Cover/index.tsx
@@ -1,6 +1,5 @@
-
 import { useAccount } from "@starknet-react/core";
-import fight from '../../assets/img/banner.jpeg';
+import fight from "../../assets/img/banner.jpeg";
 import Footer from "../Footer/index.tsx";
 import SpawnBeast from "../SpawnBeast/index.tsx";
 
@@ -9,22 +8,30 @@ function Cover() {
 
   return (
     <>
-      {
-        account ? <SpawnBeast /> :
-          <>
-            <div className='cover'>
-              <div className="mb-3">
-                <img className="cover-pic" src={fight} alt="" />
-              </div>
-              <button className="connect-btn" onClick={() => (document.querySelector('.navbar-toggler') as HTMLElement)?.click()}>
-                Connect and start Play
-              </button>
-              <Footer />
+      {account ? (
+        <SpawnBeast />
+      ) : (
+        <>
+          <div className="cover">
+            <div className="mb-3">
+              <img className="cover-pic" src={fight} alt="" />
             </div>
-          </>
-      }
+            <button
+              className="connect-btn"
+              onClick={() =>
+                (
+                  document.querySelector(".navbar-toggler") as HTMLElement
+                )?.click()
+              }
+            >
+              Connect and start Play
+            </button>
+            <Footer />
+          </div>
+        </>
+      )}
     </>
-  )
+  );
 }
 
 export default Cover;
