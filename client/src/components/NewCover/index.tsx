f9318cbbfa90502834235467b4653f998cafb12b
jimenezz22 <ljimenezbogantes@gmail.com>
2025-02-03 00:42:14 -0600

    Update NewCover component to import styles from styles.css

--- client/src/components/NewCover/index.tsx
+++ client/src/components/NewCover/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import './App.css';
+import './styles.css';
 
 const VennDiagram = () => {
   const [currentCircle, setCurrentCircle] = useState('play');
