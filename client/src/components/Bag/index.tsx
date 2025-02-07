31c46926a587fc83c53344dd4db8056da69e4336
jimenezz22 <ljimenezbogantes@gmail.com>
2025-02-06 18:10:41 -0600

    [FEAT]: Integrate slider for beast display and enable HTTPS in Vite config

--- client/src/components/Bag/index.tsx
+++ client/src/components/Bag/index.tsx
@@ -1,6 +1,9 @@
 import { Link } from 'react-router-dom';
-import { useEffect, useState, useRef } from 'react';
+import { useEffect } from 'react';
 import { SDK } from "@dojoengine/sdk";
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import { Beast, SchemaType } from "../../dojo/bindings.ts";
 import { useBeast } from '../../hooks/useBeasts.tsx';
 import ControllerConnectButton from '../CartridgeController/ControllerConnectButton.tsx';
@@ -13,63 +16,52 @@ import './main.css';
 function Bag({ sdk }: { sdk: SDK<SchemaType> }) {
   const { beasts } = useBeast(sdk);
   const { account } = useAccount();
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const totalSlides = beasts.length;
-  const touchStartX = useRef<number | null>(null);
-  const touchEndX = useRef<number | null>(null);
-
-  const handleTouchStart = (e: React.TouchEvent) => {
-    touchStartX.current = e.touches[0].clientX;
-  };
-
-  const handleTouchMove = (e: React.TouchEvent) => {
-    touchEndX.current = e.touches[0].clientX;
-  };
-
-  const handleTouchEnd = () => {
-    if (touchStartX.current !== null && touchEndX.current !== null) {
-      const deltaX = touchEndX.current - touchStartX.current;
-
-      if (deltaX > 50) {
-        setCurrentSlide((prev) => Math.max(prev - 1, 0));
-      } else if (deltaX < -50) {
-        setCurrentSlide((prev) => Math.min(prev + 1, totalSlides - 1));
-      }
-    }
-
-    touchStartX.current = null;
-    touchEndX.current = null;
-  };
-
   const {
-      setup: { client },
-    } = useDojo();
+    setup: { client },
+  } = useDojo();
+
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    customPaging: function() {
+      return (
+        <div className="indicator"></div>
+      );
+    }
+  };
 
   const getSlideContent = (beast: typeof beasts[0]) => (
-    <>
-      <div className="beast-slide">
-        <div className="beast">
-          <div className="beast-header">
-            <h2>{beast.name}</h2>
-          </div>
-          <div className="beast-pic d-flex align-items-end">
-            <img src={initials[beast.specie - 1].idlePicture} alt="beast" />
-          </div>
-          <div className="initial-info">
-            <h4>
-              {initials[beast.specie - 1].name} Lvl {beast.level}
-            </h4>
-            <p>
-              Your are close to evolve {initials[beast.specie - 1].name}, keep playing to reach the next level
-            </p>
-          </div>
-          <Link to={`/play/${beast.beast_id}`} className="button" onClick={async() => {  await client.actions.setCurrentBeast(account as Account, beast.beast_id) }}>
-            PLAY
-          </Link>
+    <div className="beast-slide">
+      <div className="beast">
+        <div className="beast-header">
+          <h2>{beast.name}</h2>
         </div>
+        <div className="beast-pic d-flex align-items-end">
+          <img src={initials[beast.specie - 1].idlePicture} alt="beast" />
+        </div>
+        <div className="initial-info">
+          <h4>
+            {initials[beast.specie - 1].name} Lvl {beast.level}
+          </h4>
+          <p>
+            Your are close to evolve {initials[beast.specie - 1].name}, keep playing to reach the next level
+          </p>
+        </div>
+        <Link 
+          to={`/play/${beast.beast_id}`} 
+          className="button" 
+          onClick={async() => {
+            await client.actions.setCurrentBeast(account as Account, beast.beast_id)
+          }}
+        >
+          PLAY
+        </Link>
       </div>
-    </>
-
+    </div>
   );
 
   useEffect(() => {
@@ -91,30 +83,13 @@ function Bag({ sdk }: { sdk: SDK<SchemaType> }) {
         <ControllerConnectButton />
       </div>
       <div className="carousel">
-        <div
-          className="slides"
-          style={{ transform: `translateX(-${currentSlide * 100}%)` }}
-          onTouchStart={handleTouchStart}
-          onTouchMove={handleTouchMove}
-          onTouchEnd={handleTouchEnd}
-        >
+        <Slider {...settings}>
           {beasts.map((beast: Beast, index: number) => (
-            <div key={index} className="slide">
+            <div key={index}>
               {getSlideContent(beast)}
             </div>
           ))}
-        </div>
-        <div className="carousel-controls">
-          <div className="indicators">
-            {beasts.map((_: any, index: number) => (
-              <div
-                key={index}
-                className={`indicator ${currentSlide === index ? 'active' : ''}`}
-                onClick={() => setCurrentSlide(index)}
-              />
-            ))}
-          </div>
-        </div>
+        </Slider>
       </div>
     </div>
   );
