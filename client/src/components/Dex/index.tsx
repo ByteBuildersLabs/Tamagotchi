c09500ee288ba9dbba781c26664cc60d975fda92
jimenezz22 <ljimenezbogantes@gmail.com>
2025-02-07 02:11:48 -0600

    [FEAT]: Update DexCarousel component to use main.css and add font imports

--- client/src/components/Dex/index.tsx
+++ client/src/components/Dex/index.tsx
@@ -2,7 +2,7 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import beastsData from '../../data/dex/BeastsDex.json';
-import './DexCarousel.css';
+import './main.css';
 
 function DexCarousel() {
   const settings = {
