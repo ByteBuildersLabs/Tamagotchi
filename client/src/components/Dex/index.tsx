9daa3b85b7ba2a97ab028b1ea7574023856d17e9
jimenezz22 <ljimenezbogantes@gmail.com>
2025-02-07 01:19:54 -0600

    [FEAT]: Implement DexCarousel component for displaying BeastsDex with slider functionality

---
+++ client/src/components/Dex/index.tsx
@@ -0,0 +1,37 @@
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import beastsData from '../../data/dex/BeastsDex.json';
+import './DexCarousel.css';
+
+function DexCarousel() {
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    swipe: beastsData.BeastsDex.length > 1,
+    customPaging: function () {
+      return <div className="indicator"></div>;
+    }
+  };
+
+  return (
+    <div className="dex-container">
+      <h1>BeastsDex</h1>
+      <Slider {...settings}>
+        {beastsData.BeastsDex.map((beast, index) => (
+          <div key={index} className="beast-card">
+            <img src={beast.Image} alt={beast.Name} className="beast-image" />
+            <h2>{beast.Name}</h2>
+            <p className="beast-type">Type: {beast.BeastsType}</p>
+          </div>
+        ))}
+      </Slider>
+    </div>
+  );
+}
+
+export default DexCarousel;
