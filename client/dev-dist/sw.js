43cbbc5efebba01e841b6e3858511ba1d0c05083
jimenezz22 <ljimenezbogantes@gmail.com>
2025-02-18 19:40:39 -0600

    Fix: update revision number for index.html in service worker

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-20a2f87f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qqfgb8jjlm"
+    "revision": "0.5kni8ehjhto"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
