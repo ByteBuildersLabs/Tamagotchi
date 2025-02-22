5221187b2a7931101da7b67bbfa5dc20735ec486
Marco Araya Jiménez <marcojime23@gmail.com>
2025-02-22 15:22:37 -0700

    feat: Add available voices configuration

---
+++ client/src/config/voices.ts
@@ -0,0 +1,7 @@
+import { Voice } from '../types/types';
+
+export const AVAILABLE_VOICES: Voice[] = [
+  { id: "AMA7aVFXjoVjAov0t4LT", name: "YoungEagle" },
+  { id: "flHkNRp1BlvT73UL6gyz", name: "Foxling" },
+  { id: "eLbv2KkHlscve7cdUE1k", name: "WolfPup" }
+];
