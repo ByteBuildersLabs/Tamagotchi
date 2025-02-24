3214ca5cf388436cdf26618041eeb50ce9402ba3
Marco Araya Jiménez <marcojime23@gmail.com>
2025-02-24 15:18:18 -0700

    feat: Enhance manifest handling with type safety for deployType

--- client/src/config/manifest.ts
+++ client/src/config/manifest.ts
@@ -2,14 +2,22 @@ import slot from "../../../dojo/manifest_dev.json";
 import sepolia from "../../../dojo/manifest_dev.json";
 import mainnet from "../../../dojo/manifest_dev.json";
 
-const deployType = import.meta.env.VITE_PUBLIC_DEPLOY_TYPE;
+// Defines a type for the valid keys of manifests
+type DeployType = keyof typeof manifests;
 
+// Create an object to hold the different manifests
 const manifests = {
   mainnet,
   sepolia,
   slot,
 };
 
-export const manifest = deployType in manifests ? manifests[deployType] : slot;
+// Verify if deployType is a valid key
+const deployType = import.meta.env.VITE_PUBLIC_DEPLOY_TYPE as string;
+
+// Use the safe check to ensure deployType is a valid key
+export const manifest = deployType in manifests 
+  ? manifests[deployType as DeployType] 
+  : slot;
 
 export type Manifest = typeof manifest;
