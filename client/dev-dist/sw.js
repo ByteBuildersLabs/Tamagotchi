b9d45a7735bcab5ce2053f4d0bcd49856ca51a00
jimenezz22 <ljimenezbogantes@gmail.com>
2025-02-24 00:47:37 -0600

    [FIX] Mini-Game

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-20a2f87f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cghgc0i0hv8"
+    "revision": "0.n33u5vk8tf"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
