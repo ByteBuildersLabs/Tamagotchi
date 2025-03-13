649590bd9a1955aab1487cf48afa434c2d662945
RolandoDrRobot <rolando.arguello.email@gmail.com>
2025-03-13 14:44:34 -0600

    The beast appear

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-20a2f87f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.30ev6o4dago"
+    "revision": "0.2veobce18eo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
