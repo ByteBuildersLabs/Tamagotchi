0576c5aec76422280702fcef93c51cc01857eae8
RolandoDrRobot <rolando.arguello.email@gmail.com>
2025-03-26 20:12:09 -0600

    Fix server

--- client/src/components/ui/circularProgressBar.tsx
+++ client/src/components/ui/circularProgressBar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import useSound from 'use-sound';
 import buttonClick from '../../assets/sounds/click.mp3';
 
