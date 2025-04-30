ae71218097459481a4295ea17ccdbbd361981562
RolandoDrRobot <rolando.arguello.email@gmail.com>
2025-04-29 22:25:38 -0600

    Fix deploy and ui for buttons

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-20a2f87f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hq34nv3vfug"
+    "revision": "0.brscfk31s88"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
