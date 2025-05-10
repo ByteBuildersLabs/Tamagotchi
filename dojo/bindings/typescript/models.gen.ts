f8ab37dbd9e8138a112d6ff5e3a2ced5ef667d8f
danielcdz <calde0601@gmail.com>
2025-05-10 13:43:30 -0600

    New deploy and bindings

--- dojo/bindings/typescript/models.gen.ts
+++ dojo/bindings/typescript/models.gen.ts
@@ -88,6 +88,58 @@ export interface PlayerValue {
 	creation_day: BigNumberish;
 }
 
+// Type definition for `achievement::events::index::TrophyCreation` struct
+export interface TrophyCreation {
+	id: BigNumberish;
+	hidden: boolean;
+	index: BigNumberish;
+	points: BigNumberish;
+	start: BigNumberish;
+	end: BigNumberish;
+	group: BigNumberish;
+	icon: BigNumberish;
+	title: BigNumberish;
+	description: string;
+	tasks: Array<Task>;
+	data: string;
+}
+
+// Type definition for `achievement::events::index::TrophyCreationValue` struct
+export interface TrophyCreationValue {
+	hidden: boolean;
+	index: BigNumberish;
+	points: BigNumberish;
+	start: BigNumberish;
+	end: BigNumberish;
+	group: BigNumberish;
+	icon: BigNumberish;
+	title: BigNumberish;
+	description: string;
+	tasks: Array<Task>;
+	data: string;
+}
+
+// Type definition for `achievement::events::index::TrophyProgression` struct
+export interface TrophyProgression {
+	player_id: BigNumberish;
+	task_id: BigNumberish;
+	count: BigNumberish;
+	time: BigNumberish;
+}
+
+// Type definition for `achievement::events::index::TrophyProgressionValue` struct
+export interface TrophyProgressionValue {
+	count: BigNumberish;
+	time: BigNumberish;
+}
+
+// Type definition for `achievement::types::index::Task` struct
+export interface Task {
+	id: BigNumberish;
+	total: BigNumberish;
+	description: string;
+}
+
 // Type definition for `tamagotchi::events::push::PushToken` struct
 export interface PushToken {
 	player_address: string;
@@ -111,6 +163,13 @@ export interface SchemaType extends ISchemaType {
 		HighestScoreValue: HighestScoreValue,
 		Player: Player,
 		PlayerValue: PlayerValue,
+	},
+	achievement: {
+		TrophyCreation: TrophyCreation,
+		TrophyCreationValue: TrophyCreationValue,
+		TrophyProgression: TrophyProgression,
+		TrophyProgressionValue: TrophyProgressionValue,
+		Task: Task,
 		PushToken: PushToken,
 		PushTokenValue: PushTokenValue,
 	},
@@ -183,6 +242,48 @@ export const schema: SchemaType = {
 			last_active_day: 0,
 			creation_day: 0,
 		},
+		TrophyCreation: {
+			id: 0,
+			hidden: false,
+			index: 0,
+			points: 0,
+			start: 0,
+			end: 0,
+			group: 0,
+			icon: 0,
+			title: 0,
+		description: "",
+			tasks: [{ id: 0, total: 0, description: "", }],
+		data: "",
+		},
+		TrophyCreationValue: {
+			hidden: false,
+			index: 0,
+			points: 0,
+			start: 0,
+			end: 0,
+			group: 0,
+			icon: 0,
+			title: 0,
+		description: "",
+			tasks: [{ id: 0, total: 0, description: "", }],
+		data: "",
+		},
+		TrophyProgression: {
+			player_id: 0,
+			task_id: 0,
+			count: 0,
+			time: 0,
+		},
+		TrophyProgressionValue: {
+			count: 0,
+			time: 0,
+		},
+		Task: {
+			id: 0,
+			total: 0,
+		description: "",
+		},
 		PushToken: {
 			player_address: "",
 		token: "",
@@ -203,6 +304,11 @@ export enum ModelsMapping {
 	HighestScoreValue = 'tamagotchi-HighestScoreValue',
 	Player = 'tamagotchi-Player',
 	PlayerValue = 'tamagotchi-PlayerValue',
+	TrophyCreation = 'achievement-TrophyCreation',
+	TrophyCreationValue = 'achievement-TrophyCreationValue',
+	TrophyProgression = 'achievement-TrophyProgression',
+	TrophyProgressionValue = 'achievement-TrophyProgressionValue',
+	Task = 'achievement-Task',
 	PushToken = 'tamagotchi-PushToken',
 	PushTokenValue = 'tamagotchi-PushTokenValue',
 }
