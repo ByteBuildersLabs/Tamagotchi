c3fff9c76053c1170523f2d0201cd180f6d52d17
RolandoDrRobot <rolando.arguello.email@gmail.com>
2025-06-17 22:21:08 -0600

    new deploy

--- client/src/components/Tamagotchi/utils/fetchAge.ts
+++ client/src/components/Tamagotchi/utils/fetchAge.ts
@@ -3,7 +3,7 @@ import hexToDecimalArray from './hexToDecimalArray';
 const fetchAge = async (account: any): Promise<number[] | undefined> => {
   try {
     const response = await account?.callContract({
-      contractAddress: "0x58971d723d0100ae8393550f9166c9dad9b79799a48fc31f0d9684ef556dda9",
+      contractAddress: "0x9ab404df9549cff7771d2404d1a4c18e04b75027c78a2c7eb43547ad76c021",
       entrypoint: "get_beast_age_with_address",
       calldata: [String(account?.address)],
     });
--- client/src/config/cartridgeConnector.tsx
+++ client/src/config/cartridgeConnector.tsx
@@ -6,9 +6,9 @@ import { constants } from "starknet";
 const { VITE_PUBLIC_DEPLOY_TYPE } = import.meta.env;
 const { VITE_PUBLIC_SLOT_ADDRESS } = import.meta.env;
 
-const CONTRACT_ADDRESS_ACHIEVEMENTS = '0x1005116a48c9a8f7a6c946091e64dc8ced37962dc2bbb74355868229307c20d'
-const CONTRACT_ADDRESS_TAMAGOTCHI_SYSTEM = '0x58971d723d0100ae8393550f9166c9dad9b79799a48fc31f0d9684ef556dda9'
-const CONTRACT_ADDRESS_PLAYER_SYSTEM = '0x6b7b30fcd29c41bca831b87db67e3c8af2abb374dc2fabbbf09f4744aa22988'
+const CONTRACT_ADDRESS_ACHIEVEMENTS = '0x678958bdd51b3caa74f0d229aaa35caafed0f03c280782d2dd71c389719bbd8'
+const CONTRACT_ADDRESS_TAMAGOTCHI_SYSTEM = '0x9ab404df9549cff7771d2404d1a4c18e04b75027c78a2c7eb43547ad76c021'
+const CONTRACT_ADDRESS_PLAYER_SYSTEM = '0x3de2979483da64ed3e3067883a9dae195cbc8df991c4536116d27382c76990d'
 
 const policies: SessionPolicies = {
   contracts: {
@@ -149,7 +149,7 @@ const options: ControllerOptions = {
   colorMode,
   preset: "bytebeasts-tamagotchi",
   namespace: "tamagotchi", 
-  slot: "toriibbtest"
+  slot: "tamatoriitest"
 };
 
 const cartridgeConnector = new ControllerConnector(
